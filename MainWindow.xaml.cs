b3af3e486bf2b22157d2e2dcb6f0a66900874fe7
Alxndr06 <alexander.aulong@gmail.com>
2024-09-10 10:07:26 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PassGen
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		// -----------------------------------------------------------------//
+
+		bool usingMinCharacters = false;
+		bool usingMajCharacters = false;
+		bool usingNumbers = false;
+		bool usingSpeCharacters = false;
+
+		private void Button_Click_About(object sender, RoutedEventArgs e)
+		{
+			var about = new About();
+
+			about.Show();
+        }
+
+		private void Button_Click_Quit(object sender, RoutedEventArgs e)
+		{
+			var buttons = MessageBoxButton.YesNo;
+			var message = "Are you sure to want to quit the program ?";
+			var title = "Leave to Windows";
+			MessageBoxResult result = MessageBox.Show(message, title, buttons);
+
+			if (result == MessageBoxResult.Yes)
+			{
+				Environment.Exit(0);
+			}
+		}
+
+		private void Button_MinChars_Click(object sender, RoutedEventArgs e)
+		{
+			if (usingMinCharacters)
+			{
+				usingMinCharacters = false;
+				MinCharsLabel.Content = "Désactivé";
+				MinCharsLabel.Background = new SolidColorBrush(Colors.Red);
+			}
+			else
+			{
+				usingMinCharacters = true;
+				MinCharsLabel.Content = "Activé";
+				MinCharsLabel.Background = new SolidColorBrush(Colors.Green);
+			}
+		}
+
+		private void Btutton_MajChars_Click(object sender, RoutedEventArgs e)
+		{
+			if (usingMajCharacters)
+			{
+				usingMajCharacters = false;
+				MajCharsLabel.Content = "Désactivé";
+				MajCharsLabel.Background = new SolidColorBrush(Colors.Red);
+			}
+			else
+			{
+				usingMajCharacters = true;
+				MajCharsLabel.Content = "Activé";
+				MajCharsLabel.Background = new SolidColorBrush(Colors.Green);
+			}
+		}
+
+		private void numbersButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (usingNumbers)
+			{
+				usingNumbers = false;
+				numbersLabel.Content = "Désactivé";
+				numbersLabel.Background = new SolidColorBrush(Colors.Red);
+			}
+			else
+			{
+				usingNumbers = true;
+				numbersLabel.Content = "Activé";
+				numbersLabel.Background = new SolidColorBrush(Colors.Green);
+			}
+		}
+
+		private void charSpecsButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (usingSpeCharacters)
+			{
+				usingSpeCharacters = false;
+				speCharsLabel.Content = "Désactivé";
+				speCharsLabel.Background = new SolidColorBrush(Colors.Red);
+			}
+			else
+			{
+				usingSpeCharacters = true;
+				speCharsLabel.Content = "Activé";
+				speCharsLabel.Background = new SolidColorBrush(Colors.Green);
+			}
+		}
+
+		private void generateButton_Click(object sender, RoutedEventArgs e)
+		{
+			Passwords password = new Passwords();
+
+			password.GeneratePassword(usingMinCharacters, usingMajCharacters, usingNumbers, usingSpeCharacters);
+		}
+
+		private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+
+        }
+    }
+}
---
+++ Passwords.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PassGen
+{
+    class Passwords
+    {
+       public void GeneratePassword(bool min, bool maj, bool nmbrs, bool charSpec)
+        {
+			const int NMBRE_MDP_MAX = 10;
+			var minuscules = "abcdefghijklmnopqrstuvwxyz";
+            var majuscules = minuscules.ToUpper();
+			var chiffres = "0123456789";
+			var characteresSpeciaux = "@_-~#!?";
+			string alphabet = "";
+			string password = "";
+
+			var longueurMotDePasse = 16;
+
+			Random rand = new Random();
+            
+			if (!min && !maj && !nmbrs && !charSpec)
+			{
+				MessageBox.Show("Erreur ! Vous devez activer au moins une option pour générer votre mot de passe.");
+			}
+			else if (min && !maj && !nmbrs && !charSpec)
+			{
+				alphabet = minuscules;
+			}
+			else if (min && maj && !nmbrs && !charSpec)
+			{
+				alphabet = minuscules + majuscules;
+			}
+			else if (min && maj && nmbrs && !charSpec)
+			{
+				alphabet = minuscules + majuscules + chiffres;
+			}
+			else if (min && maj && nmbrs && charSpec)
+			{
+				alphabet = minuscules + majuscules + chiffres + characteresSpeciaux;
+			}
+				
+			int longueurAlphabet = alphabet.Length;
+
+            string[] listeMotsDePasse = new string[NMBRE_MDP_MAX];
+
+            for (int j = 0; j < NMBRE_MDP_MAX; j++) // Boucle selon le nombre de MDP choisi dans NOMBRE_MDP.
+			{
+                password = "";
+				for (int i = 0; i < longueurMotDePasse; i++)
+				{
+					int index = rand.Next(0, longueurAlphabet);
+					password += alphabet[index];
+				}
+				listeMotsDePasse[j] = password;
+            }
+            // Construire le message avec tous les mots de passe
+            string message = "Vos mots de passe générés :\n\n";
+            foreach (string mdp in listeMotsDePasse)
+            {
+                message += mdp + "\n";
+            }
+
+            // Afficher le message
+            MessageBox.Show(message);
+        }
+
+    }
+}
